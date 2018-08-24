16249909ad708453b281f69aa9ba4b9864daaafb
Артем Беляев <>
2018-08-24 14:16:37 +0500

    start project Сформирована файловая структура проекта

---
+++ ComputerDatabase/Flows/ListComputer/ListComputerTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComputerDatabase
+//
+//  Created by Артем Б on 24.08.2018.
+//  Copyright © 2018 Артем Б. All rights reserved.
+//
+
+import UIKit
+
+class ListComputerTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
